b9f62fd821734e25cbeab56acb1b2a810d3ceedd
sajib123456 <90611173+sajib123456@users.noreply.github.com>
2021-09-20 04:10:25 -0700

    Create home.dart

---
+++ home.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("App Bar"),
+          centerTitle: true,
+          leading: Icon(
+            Icons.menu,
+            color: Colors.black,
+            size: 50.0,
+          ),
+          actions: [
+            Icon(
+              Icons.more_horiz,
+              color: Colors.yellow,
+              size: 50.0,
+            ),
+            Icon(
+              Icons.access_alarm,
+              color: Colors.red,
+            )
+          ]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              color: Colors.red,
+              child: Text(
+                "Container 1",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                color: Colors.green,
+                child: Text(
+                  "Container 2",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              color: Colors.amber,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                    color: Colors.red,
+                    child: Text(
+                      "Container 1",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                    color: Colors.green,
+                    child: Text(
+                      "Container 2",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                    color: Colors.blue,
+                    child: Text(
+                      "Container 3",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                color: Colors.black,
+                child: Text(
+                  "Container END",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
